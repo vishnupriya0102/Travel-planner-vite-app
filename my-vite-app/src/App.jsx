133f02bf6b9e4216586d9cc118902a7807ab2bc3
Vishnupriya <vishnupriya01022004@gmail.com>
2025-04-06 12:31:48 +0530

    first commit

---
+++ my-vite-app/src/App.jsx
@@ -0,0 +1,13 @@
+import {BrowerRouter} from 'react-router-dom';
+function App() {
+  return (
+    <div className="text-center">
+      <Brower
+      <h1 className="text-4xl font-bold text-red-500">
+        Hello, Vite + React + Tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
